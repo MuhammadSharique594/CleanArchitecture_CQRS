8ed277411fdd6ef7fde627c4ad493346d618e7a7
Muhammad Sharique <muhammad.sharique@psw.gov.pk>
2023-09-20 15:12:47 +0500

    Enabling swagger

--- Microservice/Login/LoginAPI/Program.cs
+++ Microservice/Login/LoginAPI/Program.cs
@@ -18,11 +18,14 @@ builder.Services.AddApplication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
